5213d2451d34e7af19c4229b75069a2d590b525d
Ayush Kumar Sharma <ayush.sharma@unthinkable.co>
2025-07-16 18:42:03 +0530

    change in cors to gave origin to all host using '*'

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 };
